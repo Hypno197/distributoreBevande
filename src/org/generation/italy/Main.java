226f2a555d92a0cba3d5b76776d7a99f8a35cb86
Luca_ <Luca_@Hypno>
2024-05-16 12:33:55 +0200

    v0.2 added funzionalità

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,5 +1,6 @@
 package org.generation.italy;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String sceltaBevanda, sceltaBeneficenza;
-		String conferma = "no";
+		String sceltaBevanda, sceltaBeneficenza, conferma = "no", password, codiceSicurezza = "ciao123.", adminSel,
+				continua;
 		String[] bevandeCalde = { "Caffè", "Cioccolata calda", "Cioccolata fondente", "Ginseng", "Ginseng amaro",
 				"Orzo", "Cappuccino", "Caffèlatte", "Latte macchiato", "Caffè alla nocciola", "The al limone",
 				"The alla pesca", "The verde", "Acqua calda" };
@@ -17,20 +18,21 @@ public class Main {
 		String[] bevandeFredde = { "Loca-Coca", "Loca Zero", "Sprint", "Fantasy", "Primathè Limone", "Primathè Pesca",
 				"Acqua Liscia", "Acqua Frizzante", "Bed Rull", "Chin9", "Birra Analcolica", "Ratogade Mirtillo&Pesca",
 				"Ratogade Arancia" };
+		ArrayList<String> vendite = new ArrayList<String>();
 		int[] giacenzaBevFredde = { 50, 50, 50, 50, 50, 50, 50, 50, 50, 0, 50, 50, 50 };
 		double[] prezziBevandeFredde = { 1.5, 1.5, 1.5, 1.5, 1.2, 1.2, 0.5, 0.5, 3, 1.5, 3, 2, 2 };
-		int codiceUser = 0;
+		int codiceUser = 0, venditaNum = 0;
 		double importo = 0;
 		float creditoInserito = 0;
 		double creditoResiduo = 0;
 		double donazione = 0;
-
+		boolean checkPw = false;
 		// inizio codice
 		// scelta bevanda calda/fredda
 		do {
 			System.out.println("Benvenuto al distributore di bevande");
 			System.out.println("\r\n"
-					+ "     _______..___  ___.      ___      .______      .___________..______    __  .___________. _______      _______.™\r\n"
+					+ "     _______..___  ___.      ___      .______      .___________..______    __  .___________. _______      _______.\r\n"
 					+ "    /       ||   \\/   |     /   \\     |   _  \\     |           ||   _  \\  |  | |           ||   ____|    /       |\r\n"
 					+ "   |   (----`|  \\  /  |    /  ^  \\    |  |_)  |    `---|  |----`|  |_)  | |  | `---|  |----`|  |__      |   (----`\r\n"
 					+ "    \\   \\    |  |\\/|  |   /  /_\\  \\   |      /         |  |     |   _  <  |  |     |  |     |   __|      \\   \\    \r\n"
@@ -39,14 +41,90 @@ public class Main {
 					+ "                                                                                                                  \r\n"
 					+ "");
 			System.out.println("Prova la nuova Ratogade al mirtillo e pesca!");
-			do {
-				do {
+			do { // scelta bevanda un'altra
+				do { // verifica input bevanda calda/fredda
 					System.out.println("Scegli fra bevanda calda o fredda!");
 					sceltaBevanda = sc.nextLine();
 					if (sceltaBevanda.equalsIgnoreCase("calda") || sceltaBevanda.equalsIgnoreCase("fredda"))
 						System.out.println("Perfetto! Ora scegli la tua bevanda!");
-					else
+
+					// scelta gestore e inserimento password
+					else if (sceltaBevanda.equalsIgnoreCase("gestore420")) {
+						do {
+							System.out.println("Inserire password");
+							password = sc.nextLine();
+							if (password.equals(codiceSicurezza))
+								checkPw = true;
+						} while (!checkPw);
+						// lettura giacenze bevande e avviso
+						for (int i = 0; i < giacenzaBevCalde.length; i++)
+							if (giacenzaBevCalde[i] < 10)
+								System.out.println(bevandeCalde[i] + " è in esaurimento.");
+						for (int i = 0; i < giacenzaBevFredde.length; i++)
+							if (giacenzaBevFredde[i] < 10)
+								System.out.println(bevandeFredde[i] + " è in esaurimento.");
+						// lettura registro malfunzionamenti?
+						System.out.println("Benvenuto nel menù di gestione del distributore.");
+						do {
+							System.out.println("Scegli fra: Prezzi, Giacenze, Vendite, Fiscale, Esci.");
+							adminSel = sc.nextLine();
+							if (adminSel.equalsIgnoreCase("prezzi")) {
+								for (int i = 0; i < bevandeCalde.length; i++)
+									System.out.println(bevandeCalde[i] + "prezzo: " + prezziBevandeCalde[i]);
+								for (int i = 0; i < bevandeFredde.length; i++)
+									System.out.println(bevandeFredde[i] + "prezzo: " + prezziBevandeFredde[i]);
+								System.out.println("C'è bisogno di effettuare modifiche?");
+								String select = sc.nextLine();
+								if (select.equalsIgnoreCase("si")) {
+									do {
+										System.out.println("Scegliere la bevanda da modificare");
+										String bevMod = sc.nextLine();
+										System.out.println("Prezzo da inserire");
+										int prezzoMod = sc.nextInt();
+										sc.nextLine();
+										for (int i = 0; i < bevandeCalde.length; i++)
+											if (bevMod.equalsIgnoreCase(bevandeCalde[i]))
+												prezziBevandeCalde[i] = prezzoMod;
+										for (int i = 0; i < bevandeFredde.length; i++)
+											if (bevMod.equalsIgnoreCase(bevandeFredde[i]))
+												prezziBevandeFredde[i] = prezzoMod;
+										System.out.println("Continuare con le modifiche?");
+										continua = sc.nextLine();
+									} while (continua.equals("si"));
+								}
+
+							}
+							if (adminSel.equalsIgnoreCase("giacenze")) {
+								for (int i = 0; i < giacenzaBevCalde.length; i++)
+									System.out
+											.println(bevandeCalde[i] + " " + giacenzaBevCalde[i] + " unità rimanenti.");
+								for (int i = 0; i < giacenzaBevFredde.length; i++)
+									System.out.println(
+											bevandeFredde[i] + " " + giacenzaBevFredde[i] + " unità rimanenti.");
+								do {
+									System.out.println("Scegliere la bevanda da caricare");
+									String bevCar = sc.nextLine();
+									System.out.println("Quantità caricata:");
+									int giacMod = sc.nextInt();
+									sc.nextLine();
+									for (int i = 0; i < bevandeCalde.length; i++)
+										if (bevCar.equals(bevandeCalde[i]))
+											giacenzaBevCalde[i] += giacMod;
+									for (int i = 0; i < bevandeFredde.length; i++)
+										if (bevCar.equals(bevandeFredde[i]))
+											giacenzaBevFredde[i] += giacMod;
+									System.out.println("Continuare con i carichi?");
+									continua = sc.nextLine();
+								} while (continua.equals("si"));
+							}
+							if (adminSel.equalsIgnoreCase("vendite")) {
+								System.out.println("registro vendite" + vendite.toString());
+							}
+						} while (!adminSel.equalsIgnoreCase("esci"));
+
+					} else
 						System.out.println("Selezione non valida!");
+
 				} while (!sceltaBevanda.equalsIgnoreCase("calda") && !sceltaBevanda.equalsIgnoreCase("fredda"));
 				// mostra listino bevande
 				if (sceltaBevanda.equalsIgnoreCase("calda")) {
@@ -62,7 +140,7 @@ public class Main {
 					}
 				}
 				// selezione prodotto
-				do {
+				do { // bevanda terminata
 					do { // ciclo verifica codice inserito
 						System.out.println("Scegli la tua bevanda");
 						codiceUser = sc.nextInt();
@@ -71,7 +149,7 @@ public class Main {
 								|| sceltaBevanda.equalsIgnoreCase("fredda") && codiceUser > bevandeFredde.length)
 							System.out.println("Codice non corretto!!");
 					} while (sceltaBevanda.equalsIgnoreCase("calda") && codiceUser > bevandeCalde.length
-							|| sceltaBevanda.equalsIgnoreCase("fredda") && codiceUser > bevandeCalde.length);
+							|| sceltaBevanda.equalsIgnoreCase("fredda") && codiceUser > bevandeFredde.length);
 
 					if (sceltaBevanda.equalsIgnoreCase("calda") && giacenzaBevCalde[codiceUser - 1] > 0) {
 						System.out.println("Hai scelto " + bevandeCalde[codiceUser - 1] + ". Inserisci "
@@ -99,6 +177,17 @@ public class Main {
 				} while (creditoResiduo < 0);
 				creditoInserito = 0;
 				System.out.println("Ecco la tua bevanda!");
+				if (sceltaBevanda.equalsIgnoreCase("calda")) {
+					vendite.add("vendita num " + venditaNum + " prodotto: " + bevandeCalde[codiceUser - 1]
+							+ " importo: " + prezziBevandeCalde[codiceUser - 1]);
+					venditaNum++;
+				}
+				if (sceltaBevanda.equalsIgnoreCase("fredda")) {
+					vendite.add("vendita num " + venditaNum + " prodotto: " + bevandeFredde[codiceUser - 1]
+							+ " importo: " + prezziBevandeFredde[codiceUser - 1]);
+					venditaNum++;
+				}
+
 				System.out.println("Vuoi prendere un'altra bevanda?");
 				conferma = sc.nextLine();
 			} while (conferma.equalsIgnoreCase("si"));
